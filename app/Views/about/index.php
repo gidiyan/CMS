945b409fd71d1753f63ac68caf78423f7f7adcdd
gidiyan <70430920+gidiyan@users.noreply.github.com>
2020-10-25 09:55:16 +0200

    first commit

--- app/Views/about/index.php
+++ app/Views/about/index.php
@@ -1,2 +1,45 @@
-<h1>About page</h1>
-<h2><?=$title;?></h2>
+<div class="row">
+    <div class="tb-3">
+        <form class="text-center border p-5 m-auto" action="">
+            <h4 class="px-4 ">Contact us</h4>
+            <div class="mb-1">
+                <label for="username"></label><input class="form-control text-center" type="text" name="username" value="" id="username" placeholder="Enter Your Name"></div>
+            <div class="mb-1">
+                <label for="email"></label><input class="form-control text-center" type="email" name="email" value="" id="email" placeholder="Enter Your Email">
+            </div>
+            <div class="form-control form-radio mb-1">
+                <label>
+                    <input name="gender" type="radio" ><strong class="text-col-5">Mr.</strong>
+                </label>
+                <label>
+                    <input name="gender" type="radio" ><strong class="text-col-5">Ms.</strong>
+                </label>
+            </div>
+            <label class="text-col-5 text_uppercase" for="">Subject:</label>
+            <select name="subject" class="form-select" id="">
+                <option class="text-uppercase" value="" disabled>Choose options</option>
+                <option  class="text-uppercase" value="1">Feedback</option>
+                <option  class="text-uppercase" value="2">Report a bug</option>
+                <option  class="text-uppercase" value="3">Feater request</option>
+            </select>
+            <div class="form-group">
+                <textarea name="message" class="form-control bg-white" id="текс" cols="20" rows="10"></textarea>
+            </div>
+            <div class="form-checkbox mb-1">
+                <label>
+                    <input type="checkbox">
+                </label>Send me a copy of this message
+            </div>
+            <button type="submit" class="btn btn-dark btn-block">Send</button>
+        </form>
+    </div>
+    <div class="tb-3"><h2>~ here could be your advertise ~</h2></div>
+    <div class="tb-3 my-2">
+        <h4 class="px-4 ">Adress</h4>
+        <p class="border-dark">
+            Location	Mountain View, California, United States</br>
+            Coordinates	37.422°N 122.084°WCoordinates: 37.422°N 122.084°W</br>
+            Address	1600 Amphitheatre Parkway</br>
+            Mountain View, CA 94043</p>
+    </div>
+</div>
